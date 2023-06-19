0f2cf4a89f88d731c18b8351c9f4953436476c5b
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-06-19 21:57:03 +0100

    physic,sound, tags
    fixed gravity and drag, added boost sound , different collision tags result in different actions

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) 
+    {
+        Debug.Log("done");
+        switch (other.gameObject.tag)
+        {
+            case "friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("Congrats, yo, you finished!");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fuel");
+                break;
+            default:
+                Debug.Log("Sorry, you blew up!");
+                break;
+        }
+    }
+}
