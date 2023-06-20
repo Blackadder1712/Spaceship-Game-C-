69bb05611a4c3ed03eaf9a4963d66cb02a03fc35
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-06-20 11:48:17 +0100

    level reload, next scene
    level reloads on player fail, working on next scene/level

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; //restarting/loading scene
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) 
+    {
+        Debug.Log("done");
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("Congrats, yo, you finished!");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fuel");
+                break;
+            default:
+                Debug.Log("Sorry, you blew up!");
+                ReloadLevel();
+              
+                break;
+        }
+    }
+
+    void ReloadLevel()//reloads level on fail 
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//return index of current scene
+    }
+}
