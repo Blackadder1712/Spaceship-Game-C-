ab0e3e14fcf5345da3c0983fcedf48bb12a08a22
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-06-21 21:19:21 +0100

    sound effects, modal
    sound effects triggering on/off as needed , 3d model of rocket imported

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,6 +8,9 @@ public class CollisionHandler : MonoBehaviour
     [SerializeField] AudioClip Win;
      AudioSource bang;
      AudioSource Complete;
+
+    bool isTransitioning = false;//stop audio on collision/explosion (true/false)
+
     void Start()
     {
        bang = GetComponent<AudioSource>();   
@@ -15,30 +18,37 @@ public class CollisionHandler : MonoBehaviour
     }
     void OnCollisionEnter(Collision other) 
     {
-        Debug.Log("done");
-        switch (other.gameObject.tag)
+        if(!isTransitioning) //only if sounds not already active 
         {
-            case "Friendly":
-                Debug.Log("This thing is friendly");
-                break;
-            case "Finish":
-                Debug.Log("Congrats, yo, you finished!");
-                Complete.PlayOneShot(Win);
-                StartNextLevel();
-                break;
-            case "Fuel":
-                Debug.Log("You picked up fuel");
-                break;
-            default:
-                Debug.Log("Sorry, you blew up!");
-                StartCrashSequence();// 1 sec delay
-              
-                break;
-        }
+            
+            switch (other.gameObject.tag)
+            {
+                case "Friendly":
+                    Debug.Log("This thing is friendly");
+                    break;
+                case "Finish":
+                    Debug.Log("Congrats, yo, you finished!");
+                    Complete.PlayOneShot(Win);
+                    StartNextLevel();
+                    break;
+                case "Fuel":
+                    Debug.Log("You picked up fuel");
+                    break;
+                default:
+                    Debug.Log("Sorry, you blew up!");
+                    StartCrashSequence();// 1 sec delay
+                
+                    break;
+            }
+
+        }   
+
     }
 
     void StartCrashSequence()
     {
+        isTransitioning = true;
+        bang.Stop();//stop after crash
         bang.PlayOneShot(Explode);
         GetComponent<movement>().enabled = false;//remove control
          Invoke("ReloadLevel", levelLoadDelay);
@@ -47,6 +57,7 @@ public class CollisionHandler : MonoBehaviour
 
     void ReloadLevel()//reloads level on fail 
     {
+       
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//return index of current scene
     }
@@ -66,6 +77,9 @@ public class CollisionHandler : MonoBehaviour
 
     void StartNextLevel()
     {
+        isTransitioning = true;
+        Complete.Stop(); //stop all audio on page load
+        Complete.PlayOneShot(Win);
          GetComponent<movement>().enabled = false;
         Invoke("NextLevel", levelLoadDelay);
     }
